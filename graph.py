0d75f42824e176183b2abc445c4b0de68bddf5f0
Ivan Vladimir Meza Ruiz <ivanvladimir@gmail.com>
2016-06-26 23:19:01 -0500

    Extra graphs

--- graph.py
+++ graph.py
@@ -19,7 +19,6 @@ cmap = plt.get_cmap('gnuplot')
 colors = [cmap(i) for i in np.linspace(0, 1, len(geometry))]
 
 df1=df.groupby(['Year','Number of microphones']).size().reset_index(name='count')
-print df1
 
 volumen=[ (20*x)^2 for x in df1['count']]
 
@@ -31,5 +30,30 @@ ax.set_xlim([1988,2018])
 ax.set_ylim([0.9,72])
 ax.set_yticks([1,2,3,4,8,16,32,64])
 ax.yaxis.set_major_formatter(ticker.FuncFormatter(myLogFormat))
+plt.legend(loc=2)
+plt.show()
+
+df1=df.groupby(['Year','Number of sources located in practice']).size().reset_index(name='count')
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ax.scatter(df1['Year'],df1['Number of sources located in practice'], s=volumen)
+ax.set_xlim([1988,2018])
+ax.set_ylim([0,8])
+ax.set_yticks([0,1,2,3,4,5,6,7,8])
+#ax.yaxis.set_major_formatter(ticker.FuncFormatter(myLogFormat))
+plt.legend(loc=2)
+plt.show()
+
+
+print df['Maximum distance to source in practice'].unique()
+
+df1=df.groupby(['Year','Maximum distance to source in practice']).size().reset_index(name='count')
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ax.scatter(df1['Year'],df1['Maximum distance to source in practice'], s=volumen)
+ax.set_xlim([1988,2018])
+ax.set_ylim([0,8])
+ax.set_yticks([0,1,2,3,4,5,6,7,8])
+#ax.yaxis.set_major_formatter(ticker.FuncFormatter(myLogFormat))
 plt.legend(loc=2)
 plt.show()
